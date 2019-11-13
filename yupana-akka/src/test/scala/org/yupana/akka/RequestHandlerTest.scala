cc755160ddbc4f55a63c754fd946a7202c33b074
Mikhail Limansky <mlimansky@energocomm.ru>
2019-11-13 19:42:32 +0300

    BDS-0: Add query info tests

--- yupana-akka/src/test/scala/org/yupana/akka/RequestHandlerTest.scala
+++ yupana-akka/src/test/scala/org/yupana/akka/RequestHandlerTest.scala
@@ -1,11 +1,14 @@
 package org.yupana.akka
 
 import com.google.protobuf.ByteString
+import org.joda.time.DateTime
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{ EitherValues, FlatSpec, Inside, Matchers }
 import org.yupana.api.Time
 import org.yupana.api.query.Query
 import org.yupana.api.types.Writable
+import org.yupana.core.dao.{ QueryMetricsFilter, TsdbQueryMetricsDao }
+import org.yupana.core.model.{ MetricData, QueryStates, TsdbQueryMetrics }
 import org.yupana.core.{ QueryContext, TSDB, TsdbServerResult }
 import org.yupana.proto.util.ProtocolVersion
 import org.yupana.proto._
@@ -86,10 +89,10 @@ class RequestHandlerTest extends FlatSpec with Matchers with MockFactory with Ei
     )
 
     val data = resp.next()
-    data shouldEqual (Response(
+    data shouldEqual Response(
       Response.Resp
         .Result(ResultChunk(Seq(ByteString.copyFrom(implicitly[Writable[String]].write("деталь от паровоза")))))
-    ))
+    )
 
     resp.next() shouldEqual Response(Response.Resp.ResultStatistics(ResultStatistics(-1, -1)))
 
@@ -111,6 +114,89 @@ class RequestHandlerTest extends FlatSpec with Matchers with MockFactory with Ei
     val resp = Await.result(requestHandler.handleQuery(tsdb, query), 2.seconds).right.value.toList
 
     resp should have size SchemaRegistry.defaultSchema.tables.size + 2 // Header and footer
+  }
+
+  class MockedTsdb(metricsDao: TsdbQueryMetricsDao) extends TSDB(null, metricsDao, null, identity)
+
+  it should "handle show queries request" in {
+    val metricsDao = mock[TsdbQueryMetricsDao]
+    val tsdb = new MockedTsdb(metricsDao)
+
+    val metrics = Seq(
+      "create_dimensions_filters",
+      "create_scans",
+      "scan",
+      "load_tags",
+      "filter_rows",
+      "window_functions_check",
+      "window_functions",
+      "map_operation",
+      "post_map_operation",
+      "reduce_operation",
+      "post_filter",
+      "collect_result_rows",
+      "dimension_values_for_ids",
+      "read_external_links",
+      "extract_data_computation",
+      "parse_scan_result"
+    )
+
+    (metricsDao.queriesByFilter _)
+      .expects(None, Some(3))
+      .returning(
+        Seq(
+          TsdbQueryMetrics(
+            1,
+            "323232",
+            new DateTime(2019, 11, 13, 0, 0),
+            0,
+            "SELECT kkm FROM kkm_items",
+            QueryStates.Running,
+            "standalone",
+            metrics.zipWithIndex.map { case (m, i) => m -> MetricData(i, i * 5d, i * 7d) }.toMap
+          )
+        )
+      )
+    val query = SqlQuery("SHOW QUERIES LIMIT 3")
+    val resp = Await.result(requestHandler.handleQuery(tsdb, query), 2.seconds).right.value.toList
+
+    resp should have size 3
+    val fields = resp(0).getResultHeader.fields.map(_.name)
+
+    fields should contain theSameElementsAs metrics.flatMap(m => Seq(s"${m}_count", s"${m}_time", s"${m}_speed")) ++ Seq(
+      "query_id",
+      "engine",
+      "state",
+      "query",
+      "start_date",
+      "total_duration"
+    )
+  }
+
+  it should "handle kill query request" in {
+    val metricsDao = mock[TsdbQueryMetricsDao]
+    val tsdb = new MockedTsdb(metricsDao)
+
+    (metricsDao.setQueryState _).expects(QueryMetricsFilter(None, Some("12345"), None), QueryStates.Cancelled)
+    val query = SqlQuery("KILL QUERY WHERE query_id = '12345'")
+    val resp = Await.result(requestHandler.handleQuery(tsdb, query), 2.seconds).right.value.toList
+
+    resp(1) shouldEqual Response(
+      Response.Resp.Result(ResultChunk(Seq(ByteString.copyFrom(implicitly[Writable[String]].write("OK")))))
+    )
+  }
+
+  it should "handle delete query metrics request" in {
+    val metricsDao = mock[TsdbQueryMetricsDao]
+    val tsdb = new MockedTsdb(metricsDao)
+
+    (metricsDao.deleteMetrics _).expects(QueryMetricsFilter(None, None, Some(QueryStates.Cancelled))).returning(8)
+    val query = SqlQuery("DELETE QUERIES WHERE state = 'CANCELLED'")
+    val resp = Await.result(requestHandler.handleQuery(tsdb, query), 2.seconds).right.value.toList
+
+    resp(1) shouldEqual Response(
+      Response.Resp.Result(ResultChunk(Seq(ByteString.copyFrom(implicitly[Writable[Int]].write(8)))))
+    )
   }
 
 }
