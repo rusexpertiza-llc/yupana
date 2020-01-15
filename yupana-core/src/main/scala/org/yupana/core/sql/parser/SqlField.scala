005169c9effca4cb5d5cfac9923784f3d59f1a21
Mikhail Limansky <mlimansky@energocomm.ru>
2020-01-15 17:02:48 +0300

    BDS-1001: Drop unused code

--- yupana-core/src/main/scala/org/yupana/core/sql/parser/SqlField.scala
+++ yupana-core/src/main/scala/org/yupana/core/sql/parser/SqlField.scala
@@ -17,7 +17,3 @@
 package org.yupana.core.sql.parser
 
 case class SqlField(expr: SqlExpr, alias: Option[String] = None)
-
-object LowercaseFieldName {
-  def unapply(fn: FieldName): Option[String] = Some(fn.lowerName)
-}
