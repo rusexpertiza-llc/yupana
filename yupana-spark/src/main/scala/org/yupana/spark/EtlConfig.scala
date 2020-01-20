0a94b8dc4a4aa9ed809ef38ada3d579ccf3f85b8
Mikhail Limansky <mlimansky@energocomm.ru>
2020-01-20 12:27:55 +0300

    BDS-1001: Do not make putIntuExternalLinks constructor parameter

--- yupana-spark/src/main/scala/org/yupana/spark/EtlConfig.scala
+++ yupana-spark/src/main/scala/org/yupana/spark/EtlConfig.scala
@@ -18,9 +18,10 @@ package org.yupana.spark
 
 import org.apache.spark.SparkConf
 
-class EtlConfig(sparkConf: SparkConf, override val putIntoExternalLinks: Boolean = true) extends Config(sparkConf) {
+class EtlConfig(sparkConf: SparkConf) extends Config(sparkConf) {
   val hbaseWriteBufferSize: Option[Long] =
     sparkConf.getOption("hbase.write.buffer").map(_.trim).filter(_.nonEmpty).map(_.toLong)
 
   override val putEnabled: Boolean = true
+  override val putIntoExternalLinks: Boolean = true
 }
