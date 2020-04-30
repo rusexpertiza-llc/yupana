3fe9ed21f802126cc702b1fa4eea1e5ca6656441
VictorMikheev <victor.mikheev@gmail.com>
2020-04-30 09:49:45 +0300

    BDS-1118: spark serialization fix

--- yupana-hbase/src/main/scala/org/yupana/hbase/TSDaoHBaseBase.scala
+++ yupana-hbase/src/main/scala/org/yupana/hbase/TSDaoHBaseBase.scala
@@ -111,9 +111,10 @@ trait TSDaoHBaseBase[Collection[_]] extends TSReadingDao[Collection, Long] with 
 
     val mr = mapReduceEngine(metricCollector)
 
+    val table = query.table
     mr.batchFlatMap(rows, EXTRACT_BATCH_SIZE) { rs =>
       val filtered = context.metricsCollector.filterRows.measure(rs.size) {
-        rs.filter(r => rowFilter(HBaseUtils.parseRowKey(r.getRow, query.table)))
+        rs.filter(r => rowFilter(HBaseUtils.parseRowKey(r.getRow, table)))
       }
 
       new TSDHBaseRowIterator(context, filtered.iterator, internalRowBuilder)
