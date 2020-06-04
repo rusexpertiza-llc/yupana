956188b14798b6bf89cf2e4e1addda3e7781ac68
Mike Limansky <mike.limansky@gmail.com>
2020-06-04 17:43:53 +0300

    BDS-0: Test inverted index dao

---
+++ yupana-hbase/src/test/scala/org/yupana/hbase/DaoTestSuite.scala
@@ -0,0 +1,26 @@
+package org.yupana.hbase
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseTestingUtility
+import org.scalatest.{ BeforeAndAfterAll, FlatSpec }
+
+trait HBaseTestBase {
+  def getConfiguration: Configuration
+  def connection: ExternalLinkHBaseConnection
+}
+
+class DaoTestSuite extends FlatSpec with BTreeIndexDaoHBaseTest with InvertedIndexDaoHBaseTest with BeforeAndAfterAll {
+  private val utility = new HBaseTestingUtility
+
+  override def getConfiguration: Configuration = utility.getConfiguration
+
+  override val connection = new ExternalLinkHBaseConnection(getConfiguration, "test")
+
+  override def beforeAll(): Unit = {
+    utility.startMiniCluster(1, 1)
+  }
+
+  override protected def afterAll(): Unit = {
+    utility.shutdownMiniCluster()
+  }
+}
