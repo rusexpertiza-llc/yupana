28884b40e12a78961f81937257ce5b0acf8ebb11
Mike Limansky <mike.limansky@gmail.com>
2020-10-16 15:13:45 +0300

    BG-1517: Add aggregation syntax for metrics

--- yupana-api/src/main/scala/org/yupana/api/query/syntax/AggregationSyntax.scala
+++ yupana-api/src/main/scala/org/yupana/api/query/syntax/AggregationSyntax.scala
@@ -17,12 +17,17 @@
 package org.yupana.api.query.syntax
 
 import org.yupana.api.query._
+import org.yupana.api.schema.Metric
 
 trait AggregationSyntax {
   def sum[T](e: Expression[T])(implicit n: Numeric[T]) = SumExpr(e)
+  def sum[T](m: Metric.Aux[T])(implicit n: Numeric[T]) = SumExpr(MetricExpr(m))
   def min[T](e: Expression[T])(implicit ord: Ordering[T]) = MinExpr(e)
+  def min[T](m: Metric.Aux[T])(implicit ord: Ordering[T]) = MinExpr(MetricExpr(m))
   def max[T](e: Expression[T])(implicit ord: Ordering[T]) = MaxExpr(e)
+  def max[T](m: Metric.Aux[T])(implicit ord: Ordering[T]) = MaxExpr(MetricExpr(m))
   def count[T](e: Expression[T]) = CountExpr(e)
+  def count[T](m: Metric.Aux[T]) = CountExpr(MetricExpr(m))
   def distinctCount[T](e: Expression[T]) = DistinctCountExpr(e)
   def distinctRandom[T](e: Expression[T]) = DistinctRandomExpr(e)
 }
