75c25ce616eccd11c597ea65cf90c17762494021
Mike Limansky <mike.limansky@gmail.com>
2020-11-20 15:05:00 +0300

    BG-0: Implement a big part of TcpClient

--- yupana-jdbc/src/main/java/org/yupana/jdbc/YupanaTcpClient.java
+++ yupana-jdbc/src/main/java/org/yupana/jdbc/YupanaTcpClient.java
@@ -18,13 +18,19 @@ package org.yupana.jdbc;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ProtocolException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.SocketChannel;
 import java.util.*;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.yupana.Proto.*;
+import org.yupana.api.query.SimpleResult;
 import org.yupana.proto.util.ProtocolVersion;
 import org.yupana.api.query.Result;
 import org.yupana.api.types.DataType;
@@ -35,16 +41,16 @@ public class YupanaTcpClient implements AutoCloseable {
 
   private final String host;
   private final int port;
+  private SocketChannel channel;
+
+  private static final int CHUNK_SIZE = 1024 * 100;
+  private static final Logger logger = Logger.getLogger(YupanaTcpClient.class.getName());
 
   public YupanaTcpClient(String host, int port) {
     this.host = host;
     this.port = port;
   }
 
-  private Logger logger = Logger.getLogger(YupanaTcpClient.class.getName());
-
-  private static final int CHUNK_SIZE = 1024 * 100
-  private SocketChannel channel;
 
   private void ensureConnected() throws IOException {
     if (channel == null || !channel.isConnected()) {
@@ -54,61 +60,63 @@ public class YupanaTcpClient implements AutoCloseable {
     }
   }
 
-  public Result query(String query, Map<Integer, ParamValue> params) {
+  public Result query(String query, Map<Integer, ParamValue> params) throws IOException {
     Request request = createProtoQuery(query, params);
     return execRequestQuery(request);
   }
 
-  public Result batchQuery(String query, Collection<Map<Integer, ParamValue>> params) {
+  public Result batchQuery(String query, Collection<Map<Integer, ParamValue>> params) throws IOException {
     Request request = creteProtoBatchQuery(query, params);
     return execRequestQuery(request);
   }
 
-  Optional<Version> ping(Long reqTime) {
-    Request request = createProtoPing(reqTime)
-    execPing(request) match {
-      case Right(response) =>
-        if (response.reqTime != reqTime) {
-          throw new Exception("got wrong ping response")
-        }
-        channel.close()
-        response.version
+  Optional<Version> ping(Long reqTime) throws IOException {
+    Request request = createProtoPing(reqTime);
+    Pong pong = execPing(request);
 
-      case Left(msg) => throw new IOException(msg)
+    if (pong.getReqTime() != reqTime) {
+      throw new ProtocolException("got wrong ping response");
     }
+    channel.close();
+
+    return Optional.ofNullable(pong.getVersion());
   }
 
-  private  execPing(request: Request): Either[String, Pong] = {
-    ensureConnected();
-    sendRequest(request)
-    val pong = fetchResponse()
-
-    pong.resp match {
-      case Response.Resp.Pong(r) =>
-        if (r.getVersion.protocol != ProtocolVersion.value) {
-          Left(
-            error(
-              s"Incompatible protocol versions: ${r.getVersion.protocol} on server and ${ProtocolVersion.value} in this driver"
-            )
-          )
-        } else {
-          Right(r)
-        }
-
-      case Response.Resp.Error(msg) =>
-        Left(error(s"Got error response on ping, '$msg'"))
-
-      case _ =>
-        Left(error("Unexpected response on ping"))
-
-    }
-  }
-
-  private Result execRequestQuery( Request request) {
+  private Pong execPing(Request request) throws IOException {
     ensureConnected();
     sendRequest(request);
-    val it = new FramingChannelIterator(channel, CHUNK_SIZE + 4)
-      .map(bytes => Response.parseFrom(bytes).resp)
+    Response response = fetchResponse();
+
+    switch (response.getRespCase()) {
+      case PONG:
+        Pong pong = response.getPong();
+        if (pong.getVersion().getProtocol() != ProtocolVersion.value)
+          throw new ProtocolException(
+                  String.format("Incompatible protocol versions: %d on server and %d in this driver",
+                          pong.getVersion().getProtocol(),
+                          Protocolversion.value)
+          );
+        return pong;
+
+      case ERROR:
+        throw new IOException(error(String.format("Got error response on ping, '%s'", response.getError())));
+
+      default:
+        throw new IOException(error("Unexpected response on ping"));
+    }
+  }
+
+  private Result execRequestQuery( Request request) throws IOException {
+    ensureConnected();
+    sendRequest(request);
+    FramingChannelIterator it = new FramingChannelIterator(channel, CHUNK_SIZE + 4);
+
+    try {
+      Stream<Response> resps = StreamSupport.stream(Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED), false)
+              .map(Response::parseFrom);
+    } catch (InvalidProtocolBufferException e) {
+      e.printStackTrace();
+    }
 
     val header = it.map(resp => handleResultHeader(resp)).find(_.isDefined).flatten
 
@@ -127,67 +135,78 @@ public class YupanaTcpClient implements AutoCloseable {
     }
   }
 
-  private void sendRequest( Request request) throws IOException, InterruptedException {
+  private void sendRequest( Request request) throws IOException {
     try {
       channel.write(createChunks(request.toByteArray()));
     } catch (IOException io) {
-        logger.warning(String.format("Caught %s while trying to write to channel, let's retry", io.getMessage()));
+      logger.warning(String.format("Caught %s while trying to write to channel, let's retry", io.getMessage()));
+      try {
         Thread.sleep(1000);
-        ensureConnected();
-        channel.write(createChunks(request.toByteArray()));
+      } catch (InterruptedException e) {
+        throw new IOException(e);
+      }
+      ensureConnected();
+      channel.write(createChunks(request.toByteArray()));
     }
   }
 
   private ByteBuffer[] createChunks(byte [] data) {
-    data
-      .sliding(CHUNK_SIZE, CHUNK_SIZE)
-      .map { ch =>
-        val bb = ByteBuffer.allocate(ch.length + 4).order(ByteOrder.BIG_ENDIAN)
-        bb.putInt(ch.length)
-        bb.put(ch)
-        bb.flip()
-        bb
-      }
-      .toArray
+    int last = data.length % CHUNK_SIZE;
+    int chunks = data.length / CHUNK_SIZE + (last > 0 ? 1 : 0);
+    ByteBuffer[] result  = new ByteBuffer[chunks];
+    for (int i = 0; i < chunks - (last > 0 ? 1: 0); i++) {
+      result[i] = ByteBuffer.allocate(CHUNK_SIZE + 4).order(ByteOrder.BIG_ENDIAN);
+      result[i].putInt(CHUNK_SIZE);
+      result[i].put(data, i * CHUNK_SIZE, CHUNK_SIZE);
+    }
+
+    if (last > 0) {
+      result[chunks - 1] = ByteBuffer.allocate(last + 4).order(ByteOrder.BIG_ENDIAN);
+      result[chunks - 1].putInt(last);
+      result[chunks - 1].put(data, (chunks - 1) * CHUNK_SIZE, last);
+    }
+
+    return result;
   }
 
-  private Response fetchResponse() {
-    val bb = ByteBuffer.allocate(CHUNK_SIZE + 4).order(ByteOrder.BIG_ENDIAN)
-    val bytesRead = channel.read(bb)
-    if (bytesRead < 0) throw new IOException("Broken pipe")
-    else if (bytesRead < 4) throw new IOException("Invalid server response")
-    bb.flip()
-    val chunkSize = bb.getInt()
-    val bytes = Array.ofDim[Byte](chunkSize)
-    bb.get(bytes)
-    Response.parseFrom(bytes)
+  private Response fetchResponse() throws IOException {
+    ByteBuffer bb = ByteBuffer.allocate(CHUNK_SIZE + 4).order(ByteOrder.BIG_ENDIAN);
+    int bytesRead = channel.read(bb);
+    if (bytesRead < 0) throw new IOException("Broken pipe");
+    else if (bytesRead < 4) throw new IOException("Invalid server response");
+    bb.flip();
+    int chunkSize = bb.getInt();
+    byte[] bytes = new byte[chunkSize];
+    bb.get(bytes);
+    return Response.parseFrom(bytes);
   }
 
-  private def handleResultHeader(resp: Response.Resp): Option[Either[String, ResultHeader]] = {
-    resp match {
-      case Response.Resp.ResultHeader(h) =>
-        logger.info("Received result header " + h)
-        Some(Right(h))
+  private Optional<ResultHeader> handleResultHeader(Response response) throws IOException {
+    switch (response.getRespCase()) {
+      case Response.RespCase.RESULTHEADER:
+        ResultHeader h = response.getResultHeader();
+        logger.info("Received result header " + h);
+        return Optional.of(h);
 
-      case Response.Resp.Result(_) =>
+      case Response.RespCase.RESULT:
         Some(Left(error("Data chunk received before header")))
 
-      case Response.Resp.Pong(_) =>
+      case Response.RespCase.PONG:
         Some(Left(error("Unexpected TspPong response")))
 
-      case Response.Resp.Heartbeat(time) =>
-        heartbeat(time)
-        None
+      case Response.RespCase.HEARTBEAT:
+        heartbeat(response.getHeartbeat());
+        return Optional.empty();
 
-      case Response.Resp.Error(e) =>
-        channel.close()
-        Some(Left(error(e)))
+      case Response.RespCase.ERROR:
+        channel.close();
+        Some(Left(error(e)));
 
-      case Response.Resp.ResultStatistics(_) =>
-        Some(Left(error("Unexpected ResultStatistics response")))
+      case Response.RespCase.RESULTSTATISTICS:
+        Some(Left(error("Unexpected ResultStatistics response")));
 
-      case Response.Resp.Empty =>
-        None
+      case Response.RespCase.RESP_NOT_SET:
+        return Optional.empty();
     }
   }
 
@@ -278,10 +297,10 @@ public class YupanaTcpClient implements AutoCloseable {
             ).build();
   }
 
-  private def extractProtoResult(header: ResultHeader, res: Iterator[ResultChunk]): Result = {
-    val names = header.fields.map(_.name)
+  private Result extractProtoResult(ResultHeader header, Iterator<ResultChunk> res) {
+    List<String> names = header.getFieldsList().stream().map(ResultField::getName).collect(Collectors.toList());
     val dataTypes = CollectionUtils.collectErrors(header.fields.map { resultField =>
-      DataType.bySqlName(resultField.`type`).toRight(s"Unknown type ${resultField.`type`}")
+      DataType.bySqlName(resultField.type).toRight(s"Unknown type ${resultField.type}")
     }) match {
       case Right(types) => types
       case Left(err)    => throw new IllegalArgumentException(s"Cannot read data: $err")
@@ -301,7 +320,7 @@ public class YupanaTcpClient implements AutoCloseable {
         .toArray
     }
 
-    SimpleResult(header.tableName.getOrElse("TABLE"), names, dataTypes, values)
+    return new SimpleResult(Optional.ofNullable(header.getTableName()).orElse("TABLE"), names, dataTypes, values);
   }
 
   private Request createProtoQuery(String query, Map<Integer, ParamValue> params) {
@@ -312,7 +331,7 @@ public class YupanaTcpClient implements AutoCloseable {
     return Request.newBuilder().setSqlQuery(sq).build();
   }
 
-  private Request creteProtoBatchQuery(String query, List<Map<Integer, ParamValue>> params) {
+  private Request creteProtoBatchQuery(String query, Collection<Map<Integer, ParamValue>> params) {
 
     BatchSqlQuery.Builder sbq = BatchSqlQuery.newBuilder().setSql(query);
 
