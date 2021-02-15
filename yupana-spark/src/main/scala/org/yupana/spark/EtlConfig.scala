d57809e692f07f832904909208a4d3355c47ee2e
Rustam Iksanov <riksanov@energocomm.ru>
2021-02-15 17:20:13 +0300

    BG-2092: fix

--- yupana-spark/src/main/scala/org/yupana/spark/EtlConfig.scala
+++ yupana-spark/src/main/scala/org/yupana/spark/EtlConfig.scala
@@ -25,5 +25,4 @@ class EtlConfig(sparkConf: SparkConf) extends Config(sparkConf) {
   val putIntoInvertedIndex: Boolean = sparkConf.getBoolean("tsd.etl.load-inverted-index", defaultValue = false)
 
   override val putEnabled: Boolean = true
-  override val maxRegions: Int = 500
 }
