2ccb96df8c0d940c37c69bd5de446a263018a335
Mike Limansky <mike.limansky@gmail.com>
2021-03-09 17:01:26 +0300

    BG-2143: Get rid of LocalDateTime, implement between

--- yupana-core/src/main/scala/org/yupana/core/providers/UpdatesIntervalsProvider.scala
+++ yupana-core/src/main/scala/org/yupana/core/providers/UpdatesIntervalsProvider.scala
@@ -16,12 +16,11 @@
 
 package org.yupana.core.providers
 
-import org.joda.time.{ DateTimeZone, Interval }
+import org.joda.time.Interval
 import org.yupana.api.Time
 import org.yupana.api.query.{ Result, SimpleResult }
 import org.yupana.api.types.DataType
 import org.yupana.core.FlatQueryEngine
-import org.yupana.core.sql.parser.TimestampPeriodValue
 
 object UpdatesIntervalsProvider {
   import org.yupana.core.model.UpdateInterval._
@@ -29,14 +28,8 @@ object UpdatesIntervalsProvider {
   def handleGetUpdatesIntervals(
       flatQueryEngine: FlatQueryEngine,
       tableName: String,
-      updatedAtPeriod: TimestampPeriodValue
+      updateInterval: Interval
   ): Result = {
-
-    val updateInterval =
-      new Interval(
-        updatedAtPeriod.from.value.toDateTime(DateTimeZone.UTC).getMillis,
-        updatedAtPeriod.to.value.toDateTime(DateTimeZone.UTC).getMillis
-      )
     val updatesIntervals = flatQueryEngine.getUpdatesIntervals(tableName, updateInterval)
     val data: Iterator[Array[Any]] = updatesIntervals.map { period =>
       Array[Any](
