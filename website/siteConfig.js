97a98b382489fcf261526d4d49c4a3008433b994
Mike Limansky <mike.limansky@gmail.com>
2021-06-24 14:58:21 +0300

    BG-1499: Fix project name in site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'yupana-site',
+  projectName: 'yupana',
   organizationName: 'Rusexpertiza LLC',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
