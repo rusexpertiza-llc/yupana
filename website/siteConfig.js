5d3dc141ec11ef8d398cfbea7395f7d7e276dbb7
Mike Limansky <mike.limansky@gmail.com>
2021-06-24 15:02:56 +0300

    BG-1499: Fix organization name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'yupana',
-  organizationName: 'Rusexpertiza LLC',
+  organizationName: 'rusexpertiza-llc',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
