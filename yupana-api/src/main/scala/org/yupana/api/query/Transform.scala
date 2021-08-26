ed8724eef051306e98246bf209375ad8a04a9848
dloshkarev <DLoshkarev@energocomm.ru>
2021-08-26 11:43:05 +0300

    BG-2368: review fixes

--- yupana-api/src/main/scala/org/yupana/api/query/Transform.scala
+++ yupana-api/src/main/scala/org/yupana/api/query/Transform.scala
@@ -18,7 +18,7 @@ package org.yupana.api.query
 
 import org.yupana.api.query.Expression.Condition
 
-trait Transform
+sealed trait Transform
 
 case class Replace(in: Set[Condition], out: Condition) extends Transform
 case class Original(in: Set[Condition]) extends Transform
