1ec94c2f0c83810c58bc3efc9fc40696a8021701
idubov <idubov@energocomm.ru>
2021-11-02 11:56:53 +0300

    BG-3193: pick only most recent update interval for certain table-from-to inside one tsdb put operation

--- yupana-core/src/main/scala/org/yupana/core/dao/TSDao.scala
+++ yupana-core/src/main/scala/org/yupana/core/dao/TSDao.scala
@@ -27,7 +27,13 @@ trait TSDao[Collection[_], IdType] extends TSReadingDao[Collection, IdType] {
   val dataPointsBatchSize: Int
 
   def put(mr: MapReducible[Collection], dataPoints: Collection[DataPoint], username: String): Seq[UpdateInterval] = {
-    mr.materialize(mr.distinct(mr.batchFlatMap(dataPoints, dataPointsBatchSize)(putBatch(username)))).distinct
+    val updateIntervalsCollection = mr.batchFlatMap(dataPoints, dataPointsBatchSize)(putBatch(username))
+    val updateIntervalsByWhatUpdated = mr.map(updateIntervalsCollection)(i => i.whatUpdated -> i)
+    val updateIntervalsNormalized =
+      mr.map(
+        mr.reduceByKey(updateIntervalsByWhatUpdated)((i1, i2) => if (i1.updatedAt.isAfter(i2.updatedAt)) i1 else i2)
+      )(_._2)
+    mr.materialize(updateIntervalsNormalized).distinct
   }
 
   def putBatch(username: String)(dataPointsBatch: Seq[DataPoint]): Seq[UpdateInterval]
