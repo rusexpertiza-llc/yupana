f1a8aba21c8bf196c1018242d1121b28cb7e5859
Mike Limansky <mike.limansky@gmail.com>
2022-07-01 14:23:42 +0300

    BG-4194: Start query state saving implementation

---
+++ yupana-spark/src/main/scala/org/yupana/spark/HDFSProgressSaver.scala
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2019 Rusexpertiza LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yupana.spark
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.spark.Partition
+import org.yupana.api.query.Query
+import org.yupana.hbase.HdfsFileUtils
+
+import java.util.Properties
+import scala.io.Source
+import scala.util.Using
+
+class HDFSProgressSaver[P <: Partition](
+    fileName: String,
+    partitionStorable: PartitionStorable[P],
+    properties: Properties
+) extends ProgressSaver[P] {
+
+  private val allPartitionsHeader = "** All Partitions **"
+  private val completedPartitionsHeader = "** Completed partitions **"
+
+  override def writeHeader(q: Query): Unit = ???
+
+  override def writePartitions(ps: Seq[P]): Unit = {
+    val hBaseConfiguration = createHbaseConfiguration()
+    HdfsFileUtils.saveDataToHdfs(
+      fileName,
+      hBaseConfiguration,
+      os => {
+        os.writeBytes(allPartitionsHeader + "\n")
+        ps.foreach { partition =>
+          os.writeBytes(partitionStorable.asString(partition))
+        }
+        os.writeBytes(completedPartitionsHeader + "\n")
+      }
+    )
+  }
+
+  override def writeProgress(p: P): Unit = {
+    val hBaseConfiguration = createHbaseConfiguration()
+    HdfsFileUtils.appendDataToHdfs(
+      fileName,
+      hBaseConfiguration,
+      os => os.writeBytes(partitionStorable.asString(p))
+    )
+  }
+
+  override def readPartitions: Seq[P] = {
+    val hBaseConfiguration = createHbaseConfiguration()
+
+    val lines = HdfsFileUtils.readDataFromHdfs[List[String]](
+      fileName,
+      hBaseConfiguration,
+      is => {
+        Using.resource(Source.fromInputStream(is)) { s =>
+          s.getLines().toList
+        }
+      }
+    )
+    if (!lines.headOption.contains(allPartitionsHeader)) {
+      throw new IllegalArgumentException("Incorrect format of partitions file")
+    }
+    val (allPartitions, tail) = lines.tail.span(_ != completedPartitionsHeader)
+    val donePartitions = tail.drop(1).toSet
+    val remains = allPartitions
+      .filterNot(p => donePartitions.contains(p))
+      .zipWithIndex
+      .map {
+        case (line, index) => partitionStorable.fromString(line)
+      }
+//      .toArray
+//    logger.info(s"Partitions total: ${allPartitions.size}, remains: ${remains.length}")
+    remains
+  }
+
+  private def createHbaseConfiguration(): Configuration = {
+    val hBaseConfiguration = HBaseConfiguration.create()
+    hBaseConfiguration.set("hbase.zookeeper.quorum", properties.getProperty("hbase.zookeeper"))
+    hBaseConfiguration.set("zookeeper.session.timeout", "180000")
+    HdfsFileUtils.addHdfsPathToConfiguration(hBaseConfiguration, properties)
+    hBaseConfiguration
+  }
+}
