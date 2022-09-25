d3c904e20c5ba18cacca9edc636486e787f0bf75
Mike Limansky <mike.limansky@gmail.com>
2022-09-26 01:51:37 +0300

    BG-0: Migrate to docusaurus 2.0

---
+++ website/docusaurus.config.js
@@ -0,0 +1,83 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const siteConfig = {
+  title: 'Yupana', // Title for your website.
+  tagline: 'Yupana — система анализа розничных продаж',
+  url: 'https://docs.yupana.org', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  favicon: 'img/favicon.ico',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  customFields: {
+    repoUrl: 'https://github.com/rusexpertiza-llc/yupana',
+    cname: 'docs.yupana.org',
+  },
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          path: '../website/target/docs',
+          /* editUrl: 'https://github.com/rusexpertiza-llc/yupana/edit/master/docs/', */
+          sidebarPath: require.resolve('./sidebars.json')
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/customTheme.css')
+        }
+      })
+    ]
+  ],
+
+  organizationName: 'rusexpertiza-llc',
+  projectName: 'yupana-docs',
+
+  themeConfig:
+    ({
+      navbar: {
+        title: 'Yupana',
+        logo: {
+          alt: 'Yupana logo',
+          src: 'img/yupana-logo.png'
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'architecture',
+            position: 'right',
+            label: 'Документация'
+          },
+          {
+            href: 'https://github.com/rusexpertiza-llc/yupana',
+            label: 'GitHub',
+            position: 'right'
+          },
+          {
+            to: '/api/org/yupana/index.html',
+            label: 'API',
+            position: 'right'
+          }
+        ]
+      },
+
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © Yupana, Первый ОФД ${new Date().getFullYear()}`
+      },
+
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
+}
+
+module.exports = siteConfig;
