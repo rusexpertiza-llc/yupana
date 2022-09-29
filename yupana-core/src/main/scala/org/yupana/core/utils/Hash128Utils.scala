a2036ed8dcbbb8c8c3f21502eb3d06ee8467669e
anovikov <alnovis@gmail.com>
2022-09-29 15:36:24 +0300

    BG-3970: Fix comment for implicit

--- yupana-core/src/main/scala/org/yupana/core/utils/Hash128Utils.scala
+++ yupana-core/src/main/scala/org/yupana/core/utils/Hash128Utils.scala
@@ -16,12 +16,9 @@
 
 package org.yupana.core.utils
 
-import com.twitter.algebird.{ Hash128, MurmurHash128 }
+import com.twitter.algebird.Hash128
 import org.yupana.api.Time
 
 object Hash128Utils {
-  implicit lazy val timeHash: Hash128[Time] = new Hash128[Time] {
-    override val DefaultSeed: Long = Hash128.DefaultSeed
-    override def hashWithSeed(seed: Long, k: Time): (Long, Long) = MurmurHash128(seed)(k.millis)
-  }
+  implicit lazy val timeHash: Hash128[Time] = Hash128.longHash.contramap(_.millis)
 }
