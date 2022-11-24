d84a58bcad564b9356cbc0d59cdaff0a53334d6e
idubov <idubov@energocomm.ru>
2022-11-24 10:48:29 +0300

    Use testcontainers for HBase test

--- yupana-hbase/src/test/scala/org/yupana/hbase/HBaseUtilsTest.scala
+++ yupana-hbase/src/test/scala/org/yupana/hbase/HBaseUtilsTest.scala
@@ -118,7 +118,7 @@ class HBaseUtilsTest extends AnyFlatSpec with Matchers with MockFactory with Opt
     bb.rewind()
     bb.put(1.toByte)
       .putDouble(2.0)
-      .put(Table.DIM_TAG_OFFSET.toByte)
+      .put(Table.DIM_TAG_OFFSET)
       .putInt("test2".length)
       .put("test2".getBytes(StandardCharsets.UTF_8))
       .put((Table.DIM_TAG_OFFSET + 2).toByte)
@@ -145,13 +145,6 @@ class HBaseUtilsTest extends AnyFlatSpec with Matchers with MockFactory with Opt
     put1.getRow shouldEqual expectedRow
 
     CacheFactory.flushCaches()
-  }
-
-  it should "test" in {
-    val i1 = -1
-    val i2 = 1
-
-    Bytes.compareTo(Bytes.toBytes(i1), Bytes.toBytes(i2)) > 0 shouldEqual java.lang.Long.compareUnsigned(i1, i2) > 0
   }
 
   override protected def beforeAll(): Unit = {
