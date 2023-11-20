dec465ff1baee5817dd675962353b1be50cc48ea
Mikhail Limansky <mike.limansky@gmail.com>
2023-11-20 18:02:20 +0300

    BG-3564: Fix examples compilation

--- yupana-examples/src/main/scala/org/yupana/examples/server/Main.scala
+++ yupana-examples/src/main/scala/org/yupana/examples/server/Main.scala
@@ -102,7 +102,7 @@ object Main extends StrictLogging {
     elRegistrator.registerAll(schemaWithJson)
     logger.info("Registering catalogs done")
 
-    val ctx = new ServerContext(new RequestHandler(queryEngineRouter))
+    val ctx = new ServerContext(queryEngineRouter)
     val server = new YupanaServer(config.host, config.port, 4, ctx)
     val f = server.start()
     logger.info(s"Yupana server started, listening on ${config.host}:${config.port}")
