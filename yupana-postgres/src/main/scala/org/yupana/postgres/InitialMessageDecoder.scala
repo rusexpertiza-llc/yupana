6c5782fe834b335e9bb3ae8fab0b7feacaa70108
Mikhail Limansky <mike.limansky@gmail.com>
2024-03-05 22:26:24 +0300

    BG-5934: Make startup procedure succeed

--- yupana-postgres/src/main/scala/org/yupana/postgres/InitialMessageDecoder.scala
+++ yupana-postgres/src/main/scala/org/yupana/postgres/InitialMessageDecoder.scala
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2019 Rusexpertiza LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.yupana.postgres
 
 import com.typesafe.scalalogging.StrictLogging
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ReplayingDecoder
-import org.yupana.postgres.protocol.{ Message, SSLRequest }
+import org.yupana.postgres.protocol.{ Message, SSLRequest, StartupMessage }
 
 import java.nio.charset.{ Charset, StandardCharsets }
 import java.util
@@ -13,7 +29,9 @@ class InitialMessageDecoder extends ReplayingDecoder[Message] with StrictLogging
 
   def readNullTerminatedString(in: ByteBuf, charset: Charset): String = {
     val toEnd = in.bytesBefore(0.toByte)
-    in.readCharSequence(toEnd, charset).toString
+    val res = in.readCharSequence(toEnd, charset).toString
+    in.readByte()
+    res
   }
 
   override def decode(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit = {
@@ -30,10 +48,17 @@ class InitialMessageDecoder extends ReplayingDecoder[Message] with StrictLogging
       val vMin = v & 0xFF
       println(s"StartupMessage, $vMaj.$vMin")
 
-      val s = readNullTerminatedString(in, StandardCharsets.US_ASCII)
-      println(s"GOT ${s}")
+      var params = Map.empty[String, String]
 
+      while (in.isReadable && in.readerIndex() < len) {
+        val k = readNullTerminatedString(in, StandardCharsets.US_ASCII)
+        if (k.nonEmpty) {
+          val v = readNullTerminatedString(in, StandardCharsets.US_ASCII)
+          params += k -> v
+        }
+      }
+      println(s"PARAMS: $params")
+      out.add(StartupMessage(params("user")))
     }
-    in.skipBytes(len - 8)
   }
 }
--- yupana-postgres/src/main/scala/org/yupana/postgres/protocol/Message.scala
+++ yupana-postgres/src/main/scala/org/yupana/postgres/protocol/Message.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Rusexpertiza LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.yupana.postgres.protocol
 
 import io.netty.buffer.ByteBuf
@@ -8,10 +24,36 @@ trait ClientMessage extends Message
 
 case object SSLRequest extends ClientMessage
 
+case class StartupMessage(user: String) extends ClientMessage
+
 trait ServerMessage extends Message {
   def write(byteBuf: ByteBuf): Unit
 }
 
 case object No extends ServerMessage {
   override def write(byteBuf: ByteBuf): Unit = byteBuf.writeByte('N'.toByte)
+}
+
+case object AuthClearTextPassword extends ServerMessage {
+  override def write(byteBuf: ByteBuf): Unit = {
+    byteBuf.writeByte('R')
+    byteBuf.writeInt(4 + 4)
+    byteBuf.writeInt(3)
+  }
+}
+
+case object AuthOk extends ServerMessage {
+  override def write(byteBuf: ByteBuf): Unit = {
+    byteBuf.writeByte('R')
+    byteBuf.writeInt(4 + 4)
+    byteBuf.writeInt(0)
+  }
+}
+
+case object ReadyForQuery extends ServerMessage {
+  override def write(byteBuf: ByteBuf): Unit = {
+    byteBuf.writeByte('Z')
+    byteBuf.writeInt(4 + 1)
+    byteBuf.writeByte('I'.toByte)
+  }
 }
