5926d2857c845786b9b9ac837143618fac88c4b0
Ilya Dubov <idubov@energocomm.ru>
2024-04-04 10:20:29 +0300

    BG-5946: remove duplicated header

--- yupana-etl/src/main/scala/org/yupana/etl/EtlHBaseContext.scala
+++ yupana-etl/src/main/scala/org/yupana/etl/EtlHBaseContext.scala
@@ -23,22 +23,6 @@ import org.yupana.hbase.{ ExternalLinkHBaseConnection, InvertedIndexDaoHBase, Se
 import org.yupana.schema.externallinks.ItemsInvertedIndex
 import org.yupana.schema.{ Dimensions, ItemDimension }
 
-/*
- * Copyright 2019 Rusexpertiza LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 abstract class EtlHBaseContext(val cfg: EtlHbaseConfig, schema: Schema) extends Serializable {
 
   def setup(tsdbInstance: TSDB): Unit = {
