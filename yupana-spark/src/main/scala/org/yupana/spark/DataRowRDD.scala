9a5fb2b2aa916201057985a3f65f316ff3387daa
Victor Mikheev <victor.mikheev@gmail.com>
2024-04-10 13:48:40 +0300

    BG-5949:scalafmt

--- yupana-spark/src/main/scala/org/yupana/spark/DataRowRDD.scala
+++ yupana-spark/src/main/scala/org/yupana/spark/DataRowRDD.scala
@@ -16,18 +16,18 @@
 
 package org.yupana.spark
 
-import java.sql.{Timestamp, Types}
+import java.sql.{ Timestamp, Types }
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{ DataFrame, Row, SparkSession }
 import org.apache.spark.sql.types._
-import org.apache.spark.{Partition, TaskContext}
-import org.yupana.api.{Blob, Time}
+import org.apache.spark.{ Partition, TaskContext }
+import org.yupana.api.{ Blob, Time }
 import org.yupana.api.query.QueryField
 import org.yupana.api.types.ArrayDataType
 import org.yupana.api.types.DataType.TypeKind
 import org.yupana.core.model.BatchDataset
-import org.yupana.core.{QueryContext, TsdbResultBase}
+import org.yupana.core.{ QueryContext, TsdbResultBase }
 
 import scala.collection.mutable.ArrayBuffer
 
