4e2ffb1ca50e148c5ce87b9e3ddb7dd9ff43cf1e
Mike Limansky <mike.limansky@gmail.com>
2025-03-11 19:20:09 +0300

    BG-5963: Update documentation according to test results

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -2,6 +2,8 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import smartyPants from 'remark-smartypants';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: 'Yupana', // Title for your website.
@@ -30,20 +32,30 @@ const config: Config = {
 
   presets: [
     [
-      'classic',
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
           path: '../website/target/docs',
           /* editUrl: 'https://github.com/rusexpertiza-llc/yupana/edit/master/docs/', */
           sidebarPath: require.resolve('./sidebars.json'),
-          remarkPlugins: [smartyPants]
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex]
         },
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/customTheme.css')
         }
-      })
+      }
     ]
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    }
   ],
 
   organizationName: 'rusexpertiza-llc',
