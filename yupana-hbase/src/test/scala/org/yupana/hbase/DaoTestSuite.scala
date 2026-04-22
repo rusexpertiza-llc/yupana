f38b7ecd4385f4850dcfc4e1c8ce4c5430b59ca3
Ilya Dubov <idubov@energocomm.ru>
2026-04-22 20:12:03 +0300

    BDS-2435: fix integration tests

--- yupana-hbase/src/test/scala/org/yupana/hbase/DaoTestSuite.scala
+++ yupana-hbase/src/test/scala/org/yupana/hbase/DaoTestSuite.scala
@@ -1,10 +1,14 @@
 package org.yupana.hbase
 
-import com.dimafeng.testcontainers.{ ForAllTestContainer, GenericContainer }
+import com.dimafeng.testcontainers.{ FixedHostPortGenericContainer, ForAllTestContainer }
 import com.typesafe.scalalogging.StrictLogging
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.scalatest.flatspec.AnyFlatSpec
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.images.builder.Transferable
+
+import scala.io.Source
 
 trait HBaseTestBase {
   def getConfiguration: Configuration
@@ -24,19 +28,31 @@ class DaoTestSuite
 
   val ImageName = "pikkvile/hbase-2.4.15-standalone:1.0.0"
 
-  val container: GenericContainer = {
+  val hbaseSiteXml: String = Source.fromResource("hbase-site.xml").getLines().mkString
+
+  val container: FixedHostPortGenericContainer = {
     logger.info("instantiating Hbase Container " + ImageName)
-    val gc = new GenericContainer(ImageName)
-    gc.container.withNetworkMode("host")
+    val gc = new FixedHostPortGenericContainer(ImageName)
+    gc.container.withExposedPorts(2181, 16010)
+    gc.container.withFixedExposedPort(16000, 16000)
+    gc.container.withFixedExposedPort(16010, 16010)
+    gc.container.withFixedExposedPort(16020, 16020)
+    gc.container.withCopyToContainer(Transferable.of(hbaseSiteXml), "/opt/hbase/conf/hbase-site.xml")
+    gc.container.waitingFor(Wait.forHttp("/").forPort(16010).forStatusCode(200))
     gc
   }
 
   override def getConfiguration: Configuration = {
     val hBaseConfiguration = HBaseConfiguration.create()
-    hBaseConfiguration.set("hbase.zookeeper.quorum", "localhost")
-    hBaseConfiguration.get("hbase.zookeeper.property.clientPort", "2181")
+    hBaseConfiguration.set("hbase.zookeeper.quorum", container.container.getHost)
+    hBaseConfiguration.set("hbase.zookeeper.property.clientPort", container.container.getMappedPort(2181).toString)
+    hBaseConfiguration.set("hbase.master.hostname", "localhost")
+    hBaseConfiguration.set("hbase.regionserver.hostname", "localhost")
+    hBaseConfiguration.set("hbase.client.retries.number", "3")
+    hBaseConfiguration.set("hbase.client.pause", "1000")
+    hBaseConfiguration.set("zookeeper.recovery.retry", "1")
     hBaseConfiguration
   }
 
-  override val connection = new ExternalLinkHBaseConnection(getConfiguration, "test")
+  override def connection = new ExternalLinkHBaseConnection(getConfiguration, "test")
 }
--- yupana-spark/src/test/scala/org/yupana/spark/SparkTestSuite.scala
+++ yupana-spark/src/test/scala/org/yupana/spark/SparkTestSuite.scala
@@ -1,8 +1,12 @@
 package org.yupana.spark
 
-import com.dimafeng.testcontainers.{ Container, ForAllTestContainer, GenericContainer }
+import com.dimafeng.testcontainers.{ FixedHostPortGenericContainer, ForAllTestContainer }
 import com.typesafe.scalalogging.StrictLogging
 import org.scalatest.flatspec.AnyFlatSpec
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.images.builder.Transferable
+
+import scala.io.Source
 
 trait SparkTestEnv {
   def getZkPort: Int
@@ -17,12 +21,19 @@ class SparkTestSuite
 
   val ImageName = "pikkvile/hbase-2.4.15-standalone:1.0.0"
 
-  override val container: Container = {
-    logger.info("Instantiating HBase Container " + ImageName)
-    val gc = new GenericContainer(ImageName)
-    gc.container.withNetworkMode("host")
+  val hbaseSiteXml: String = Source.fromResource("hbase-site.xml").getLines().mkString
+
+  val container: FixedHostPortGenericContainer = {
+    logger.info("instantiating Hbase Container " + ImageName)
+    val gc = new FixedHostPortGenericContainer(ImageName)
+    gc.container.withExposedPorts(2181, 16010)
+    gc.container.withFixedExposedPort(16000, 16000)
+    gc.container.withFixedExposedPort(16010, 16010)
+    gc.container.withFixedExposedPort(16020, 16020)
+    gc.container.withCopyToContainer(Transferable.of(hbaseSiteXml), "/opt/hbase/conf/hbase-site.xml")
+    gc.container.waitingFor(Wait.forHttp("/").forPort(16010).forStatusCode(200))
     gc
   }
 
-  override def getZkPort: Int = 2181
+  override def getZkPort: Int = container.container.getMappedPort(2181)
 }
